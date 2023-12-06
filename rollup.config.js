6586f87f127a049115dd09fe5ed97ffcf812a759
Felix Flores <felixflores@gmail.com>
2023-12-06 03:05:32 -0500

    fix minification error

--- rollup.config.js
+++ rollup.config.js
@@ -3,22 +3,19 @@ import commonjs from "@rollup/plugin-commonjs";
 import terser from "@rollup/plugin-terser";
 
 export default {
-  input: "src/index.js", // Entry point
+  input: "src/index.js",
   output: [
     {
-      file: "dist/nestml.bundle.js", // Output file
-      format: "umd", // Universal Module Definition
-      name: "nestml", // Bundle name
+      file: "dist/nestml.bundle.js",
+      format: "umd",
+      name: "nestml",
     },
     {
       file: "dist/nestml.bundle.min.js",
-      format: "iife",
-      name: "version",
+      format: "umd",
+      name: "nestml",
       plugins: [terser()],
     },
   ],
-  plugins: [
-    resolve(), // Locates modules using the Node resolution algorithm, for using third party modules in node_modules
-    commonjs(), // Converts CommonJS modules to ES6
-  ],
+  plugins: [resolve(), commonjs()],
 };
